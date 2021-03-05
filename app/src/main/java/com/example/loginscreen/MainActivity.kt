b0fbbf123d41e28ef19fd452c3db2abfaeafea40
Ruslan16021984 <carbit3333333@gmail.com>
2021-03-05 18:53:52 +0200

    commit8

--- app/src/main/java/com/example/loginscreen/MainActivity.kt
+++ app/src/main/java/com/example/loginscreen/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.loginscreen
 
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.loginscreen.di.component.ViewModelComponent
 import com.example.loginscreen.domain.UserViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
@@ -13,6 +17,13 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment_container)
+        navController.addOnDestinationChangedListener {
+                controller, destination, arguments ->
+            toolbar.title = navController.currentDestination?.label
+        }
     }
 
     override fun injectDependency(component: ViewModelComponent) {
--- app/src/main/java/com/example/loginscreen/SignInFragment.kt
+++ app/src/main/java/com/example/loginscreen/SignInFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_sign_in.*
 import kotlinx.android.synthetic.main.fragment_sign_in.view.*
 
